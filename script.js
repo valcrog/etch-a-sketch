9d902e99999d03997c9b7f491f2cada836b6efb2
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-11-14 18:06:03 +0100

    Added reset button

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
+function configGrid(size) {
+    const grid = document.getElementById('grid');
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
 function createGridDivs(size) {
     const container = document.querySelector('#grid');
+    container.innerHTML = '';
     for (let i = 1; i <= size; i++) {
         for (let j = 1; j <= size; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.classList.add(`${i}-${j}`)
+            cell.classList.add(`${i}-${j}`);
             container.appendChild(cell);
         }
     }
 }
 
-let grid = document.querySelector('#grid');
+function reset() {
+    let newSize = prompt("Enter the size of the grid", 16);
+    while (newSize > 100) {
+        alert('This grid is too big!');
+        newSize = prompt("Enter the size of the grid", 16);
+    }
+    configGrid(newSize);
+    createGridDivs(newSize);
+}
+
+const grid = document.querySelector('#grid');
 grid.addEventListener('mouseover', function(event) {
     if (event.target.classList.contains('cell')) {
         event.target.style.backgroundColor = 'black';
     }
 })
+
